95f81945bdbb1a1f1ed335d03e00ce8529b91f73
Tom Fenech <tomjwfenech@gmail.com>
2018-01-01 23:45:09 +0100

    Refactor knot hasher for use in day 14

--- day10/pt2.py
+++ day10/pt2.py
@@ -1,37 +1,37 @@
 import functools
 
-s = "102,255,99,252,200,24,219,57,103,2,226,254,1,0,69,216"
+class KnotHasher:
+    LENGTH = 256
 
-state = {
-    'string': list(range(256)),
-    'position': 0,
-    'skip': 0
-}
+    def __init__(self, input):
+        self.lengths = list(map(ord, input)) + [17, 31, 73, 47, 23]
 
-def twist(state, length):
-    string = state['string']
-    position = state['position']
-    skip = state['skip']
+        self.string = list(range(self.LENGTH))
+        self.position = 0
+        self.skip = 0
 
-    replacements = list(string[(position + i) % len(string)] for i in range(length))
+    def twist(self, length):
+        replacements = list(self.string[(self.position + i) % self.LENGTH] for i in range(length))
 
-    for i in range(length):
-        string[(position + length - i - 1) % len(string)] = replacements[i]
+        for i in range(length):
+            self.string[(self.position + length - i - 1) % self.LENGTH] = replacements[i]
 
-    return {
-        'string': string,
-        'position': position + length + skip,
-        'skip': skip + 1
-    }
+        self.position = self.position + length + self.skip
+        self.skip += 1
 
-lengths = list(map(ord, s)) + [17, 31, 73, 47, 23]
+    def dense_hash(self):
+        for i in range(64):
+            for length in self.lengths:
+                self.twist(length)
 
-for i in range(64):
-    for length in lengths:
-        state = twist(state, length)
+        sparse_hash = self.string
+        return (functools.reduce(lambda a, b: a ^ b, sparse_hash[i*16:i*16+16]) for i in range(16))
 
-sparse_hash = state['string']
 
-dense_hash = (functools.reduce(lambda a, b: a ^ b, sparse_hash[i*16:i*16+16]) for i in range(16))
+    def knot_hash(self):
+        return "".join(map(lambda x: format(x, '02x'), self.dense_hash()))
 
-print("".join(map(lambda x: format(x, '02x'), dense_hash)))
+if __name__ == "__main__":
+    s = "102,255,99,252,200,24,219,57,103,2,226,254,1,0,69,216"
+    hasher = KnotHasher(s)
+    print(hasher.knot_hash())
